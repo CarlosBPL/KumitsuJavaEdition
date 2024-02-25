d4f0dc050df9ef57ccd378a5b6d7d0a0cc2d2236
marcelis6 <123846226+marcelis6@users.noreply.github.com>
2024-02-25 12:05:22 +0100

    masz

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const test = document.querySelector("#btn");
+const magic = document.querySelector("#magia");
+
 let x = 0;
+
+test.addEventListener("click", () => { 
+    if(x == 0 ){ 
+        magic.classList.remove("p");
+        magic.classList.add("test");
+        x++;
+    } else { 
+        magic.classList.add("p");
+        magic.classList.remove("test");
+        x--;
+    }
+})
